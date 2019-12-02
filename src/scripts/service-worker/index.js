4166140cc88f06005900d55af18de731a9004558
omrilotan <omrilotan@users.noreply.github.com>
2019-12-02 09:32:44 +0200

    🤓 different home about link for installed

--- src/scripts/service-worker/index.js
+++ src/scripts/service-worker/index.js
@@ -4,6 +4,7 @@ export default () => navigator.onLine &&
 
 function cacheKey() {
 	const metaCache = document.querySelector('meta[name="cache-key-version"]');
+
 	return metaCache
 		? metaCache.getAttribute('content')
 		: Math.random().toString('36').substr(5)
