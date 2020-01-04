6571069c3abd567aff43b258f68b678478341244
Restyled.io <commits@restyled.io>
2020-01-04 22:07:59 +0000

    Restyled by prettier

--- src/sames/scripts/suggest.js
+++ src/sames/scripts/suggest.js
@@ -1,59 +1,69 @@
 (function() {
-const patch = [
-  'https://discordapp.com/api', 'webhooks', '662732577182056458',
-  'tEx0sR7n_6Dtk3V2fXd4gcR8Mn4E82pPB4XXBRJ3RLP-I-ezYxXWaLrV88M4Kv0vCFay',
-  'slack'
-];
+	const patch = [
+		"https://discordapp.com/api",
+		"webhooks",
+		"662732577182056458",
+		"tEx0sR7n_6Dtk3V2fXd4gcR8Mn4E82pPB4XXBRJ3RLP-I-ezYxXWaLrV88M4Kv0vCFay",
+		"slack"
+	];
 
-const [form] = document.forms;
+	const [form] = document.forms;
 
-form.addEventListener('submit', function sendForm(event) {
-  event.preventDefault();
-  const {target} = event;
-  if (target.hasAttribute('disabled')) {
-    return;
-  }
+	form.addEventListener("submit", function sendForm(event) {
+		event.preventDefault();
+		const { target } = event;
+		if (target.hasAttribute("disabled")) {
+			return;
+		}
 
-  const [textarea] = target;
-  const {value} = textarea;
-  if (!value) {
-    return textarea.focus();
-  }
-  [target, textarea].forEach(e => e.setAttribute('disabled', 'disabled'));
-  send(value).then(thanks).catch(
-      () => [target, textarea].forEach(e => e.removeAttribute('disabled')));
-});
+		const [textarea] = target;
+		const { value } = textarea;
+		if (!value) {
+			return textarea.focus();
+		}
+		[target, textarea].forEach(e => e.setAttribute("disabled", "disabled"));
+		send(value)
+			.then(thanks)
+			.catch(() =>
+				[target, textarea].forEach(e => e.removeAttribute("disabled"))
+			);
+	});
 
-const send = text =>
-    fetch(patch.join('/'), {method : 'POST', body : JSON.stringify({text})});
+	const send = text =>
+		fetch(patch.join("/"), { method: "POST", body: JSON.stringify({ text }) });
 
-function thanks() {
-  const div = document.querySelector('div');
-  div.style.height = div.offsetHeight + 'px';
-  setTimeout(() => { div.style.height = '0px'; }, 200);
-  const header = document.querySelector('header');
-  header.parentNode.replaceChild(
-      document.querySelector('template[name="thanks"]').content, header);
-}
+	function thanks() {
+		const div = document.querySelector("div");
+		div.style.height = div.offsetHeight + "px";
+		setTimeout(() => {
+			div.style.height = "0px";
+		}, 200);
+		const header = document.querySelector("header");
+		header.parentNode.replaceChild(
+			document.querySelector('template[name="thanks"]').content,
+			header
+		);
+	}
 })();
 
 (function() {
-function init() {
-  const textarea = document.querySelector('textarea');
-  let timer;
+	function init() {
+		const textarea = document.querySelector("textarea");
+		let timer;
 
-  function resize() {
-    textarea.style.height = 'auto';
-    textarea.style.height = textarea.scrollHeight + 'px';
-  }
+		function resize() {
+			textarea.style.height = "auto";
+			textarea.style.height = textarea.scrollHeight + "px";
+		}
 
-  function debounced() {
-    clearTimeout(timer);
-    timer = setTimeout(resize, 0);
-  }
+		function debounced() {
+			clearTimeout(timer);
+			timer = setTimeout(resize, 0);
+		}
 
-  ['change', 'cut', 'paste', 'drop', 'keydown'].forEach(
-      event => textarea.addEventListener(event, debounced, false));
-}
-window.addEventListener('load', init);
+		["change", "cut", "paste", "drop", "keydown"].forEach(event =>
+			textarea.addEventListener(event, debounced, false)
+		);
+	}
+	window.addEventListener("load", init);
 })();
--- src/scripts/glossary.js
+++ src/scripts/glossary.js
@@ -1,30 +1,32 @@
-window.addEventListener('load', function prepareGlossary() {
-  const search = document.createElement('input');
-  search.setAttribute('type', 'search');
+window.addEventListener("load", function prepareGlossary() {
+	const search = document.createElement("input");
+	search.setAttribute("type", "search");
 
-  const cards = document.querySelectorAll('.cards a');
-  [].forEach.call(cards, function setupCard(card) {
-    card.style.height = card.offsetHeight + 'px';
-    card.setAttribute('h', card.offsetHeight);
-  });
+	const cards = document.querySelectorAll(".cards a");
+	[].forEach.call(cards, function setupCard(card) {
+		card.style.height = card.offsetHeight + "px";
+		card.setAttribute("h", card.offsetHeight);
+	});
 
-  function searchChange({target : {value}}) {
-    const pattern = new RegExp(value, 'i');
+	function searchChange({ target: { value } }) {
+		const pattern = new RegExp(value, "i");
 
-    [].forEach.call(cards, function resizeCard(card) {
-      const match =
-          !value || card.innerText.toLowerCase().includes(value.toLowerCase());
+		[].forEach.call(cards, function resizeCard(card) {
+			const match =
+				!value || card.innerText.toLowerCase().includes(value.toLowerCase());
 
-      card.classList.toggle('hidden', !match);
-      card.style.height = match ? card.getAttribute('h') + 'px' : '0px';
-      card.innerHTML =
-          card.innerText.replace(pattern, match => `<mark>${match}</mark>`);
-    });
-  }
+			card.classList.toggle("hidden", !match);
+			card.style.height = match ? card.getAttribute("h") + "px" : "0px";
+			card.innerHTML = card.innerText.replace(
+				pattern,
+				match => `<mark>${match}</mark>`
+			);
+		});
+	}
 
-  search.addEventListener('keyup', searchChange);
-  search.addEventListener('change', searchChange);
-  search.addEventListener('search', searchChange);
+	search.addEventListener("keyup", searchChange);
+	search.addEventListener("change", searchChange);
+	search.addEventListener("search", searchChange);
 
-  document.body.appendChild(search);
+	document.body.appendChild(search);
 });
