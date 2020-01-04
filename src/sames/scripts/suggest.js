79fba02dda72505258e57b013c8dbcfe87ecc081
Restyled.io <commits@restyled.io>
2020-01-04 22:08:25 +0000

    😎 Autofix

--- src/sames/scripts/suggest.js
+++ src/sames/scripts/suggest.js
@@ -1,18 +1,18 @@
 (function() {
 	const patch = [
-		"https://discordapp.com/api",
-		"webhooks",
-		"662732577182056458",
-		"tEx0sR7n_6Dtk3V2fXd4gcR8Mn4E82pPB4XXBRJ3RLP-I-ezYxXWaLrV88M4Kv0vCFay",
-		"slack"
+		'https://discordapp.com/api',
+		'webhooks',
+		'662732577182056458',
+		'tEx0sR7n_6Dtk3V2fXd4gcR8Mn4E82pPB4XXBRJ3RLP-I-ezYxXWaLrV88M4Kv0vCFay',
+		'slack'
 	];
 
 	const [form] = document.forms;
 
-	form.addEventListener("submit", function sendForm(event) {
+	form.addEventListener('submit', function sendForm(event) {
 		event.preventDefault();
 		const { target } = event;
-		if (target.hasAttribute("disabled")) {
+		if (target.hasAttribute('disabled')) {
 			return;
 		}
 
@@ -21,24 +21,24 @@
 		if (!value) {
 			return textarea.focus();
 		}
-		[target, textarea].forEach(e => e.setAttribute("disabled", "disabled"));
+		[target, textarea].forEach(e => e.setAttribute('disabled', 'disabled'));
 		send(value)
 			.then(thanks)
 			.catch(() =>
-				[target, textarea].forEach(e => e.removeAttribute("disabled"))
+				[target, textarea].forEach(e => e.removeAttribute('disabled'))
 			);
 	});
 
 	const send = text =>
-		fetch(patch.join("/"), { method: "POST", body: JSON.stringify({ text }) });
+		fetch(patch.join('/'), { method: 'POST', body: JSON.stringify({ text }) });
 
 	function thanks() {
-		const div = document.querySelector("div");
-		div.style.height = div.offsetHeight + "px";
+		const div = document.querySelector('div');
+		div.style.height = div.offsetHeight + 'px';
 		setTimeout(() => {
-			div.style.height = "0px";
+			div.style.height = '0px';
 		}, 200);
-		const header = document.querySelector("header");
+		const header = document.querySelector('header');
 		header.parentNode.replaceChild(
 			document.querySelector('template[name="thanks"]').content,
 			header
@@ -48,12 +48,12 @@
 
 (function() {
 	function init() {
-		const textarea = document.querySelector("textarea");
+		const textarea = document.querySelector('textarea');
 		let timer;
 
 		function resize() {
-			textarea.style.height = "auto";
-			textarea.style.height = textarea.scrollHeight + "px";
+			textarea.style.height = 'auto';
+			textarea.style.height = textarea.scrollHeight + 'px';
 		}
 
 		function debounced() {
@@ -61,9 +61,9 @@
 			timer = setTimeout(resize, 0);
 		}
 
-		["change", "cut", "paste", "drop", "keydown"].forEach(event =>
+		['change', 'cut', 'paste', 'drop', 'keydown'].forEach(event =>
 			textarea.addEventListener(event, debounced, false)
 		);
 	}
-	window.addEventListener("load", init);
+	window.addEventListener('load', init);
 })();
